c19fecca5b3d107281e1a9d667274d4a2fdce9af
tanjiajie <371997352@qq.com>
2017-09-26 21:53:32 -0700

    added react-router 4.0 and 2 blank pages

---
+++ src/MainPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class MainPage extends Component {
+    render() {
+        return (
+            <div className="App">
+                <span>啊哈哈哈哈哈啊哈哈成功了</span>
+            </div>
+        );
+    }
+}
+
+
+
+export default MainPage;
--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,20 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import React, { Component } from 'react';
 import App from './App.js'
+import MainLayout from './MainLayout';
+import MainPage from './MainPage';
 
 class APPRouter extends Component{
 
     render() {
         return (
             <Router>
-                <Route path="/" component={App}/>
+                <div>
+                    <MainLayout>
+                        <Route exact path="/" component={App}/>
+                        <Route path="/app1" component={MainPage}/>
+                    </MainLayout>
+                </div>
             </Router>
             )
 
